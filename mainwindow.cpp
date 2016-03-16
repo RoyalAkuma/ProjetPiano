4bbd6520961931dd57eb86ae5cc7b649f0a21840
Thao COURAUD <E128809K@S046V7pc14.ensinfo.sciences.univ-nantes.prive>
2016-03-16 18:06:17 +0100

    log

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,6 +16,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ofstream fichier("log.txt", ios::out);
 }
 
 
@@ -62,6 +63,7 @@ void MainWindow::on_B1Do_clicked()
 void MainWindow::on_B2Re_clicked()
 {
     sons("re2M");
+    log("reM");
 
 
 }
@@ -71,42 +73,48 @@ void MainWindow::on_B2Re_clicked()
 void MainWindow::on_B3Mi_clicked()
 {
     sons("mi2M");
+    log("miM");
 
 }
 
 void MainWindow::on_B4Fa_clicked()
 {
     sons("fa2M");
-
+    log("faM");
 }
 
 void MainWindow::on_B5Sol_clicked()
 {
     sons("sol2M");
+    log("solM");
 
 }
 
 void MainWindow::on_B6La_clicked()
 {
     sons("la2M");
+    log("laM");
 
 }
 
 void MainWindow::on_B7Si_clicked()
 {
     sons("si2M");
+    log("siM");
 
 }
 
 void MainWindow::on_B8Do2_clicked()
 {
     sons("dom");
+    log("dom");
 
 }
 
 void MainWindow::on_B9Re2_clicked()
 {
     sons("rem");
+    log("rem");
 
 }
 
@@ -114,17 +122,20 @@ void MainWindow::on_B_10Mi2_clicked()
 {
 
     sons("mim");
+    log("mim");
 }
 
 void MainWindow::on_B_11Fa2_clicked()
 {
     sons("fam");
+    log("fam");
 
 }
 
 void MainWindow::on_B_12Sol2_clicked()
 {
     sons("solm");
+    log("solm");
 
 }
 
@@ -132,59 +143,69 @@ void MainWindow::on_B_13La2_clicked()
 {
 
     sons("lam");
+    log("lam");
 }
 
 void MainWindow::on_B_14Si2_clicked()
 {
 
     sons("sim");
+    log("sim");
 }
 
 void MainWindow::on_C1_clicked()
 {
 
     sons("do2MD");
+    log("doMD");
 }
 
 void MainWindow::on_C2_clicked()
 {
     sons("re2MD");
+    log("reMD");
 
 }
 
 void MainWindow::on_C3_clicked()
 {
     sons("fa2MD");
+    log("faMD");
 
 }
 
 void MainWindow::on_C4_clicked()
 {
     sons("sol2MD");
+    log("solMD");
 
 }
 
 void MainWindow::on_C5_clicked()
 {
     sons("la2MD");
+    log("laMD");
 
 }
 
 void MainWindow::on_C6_clicked()
 {
     sons("domD");
+    log("domD");
 
 }
 
 void MainWindow::on_C7_clicked()
 {
     sons("remD");
+    log("remD");
 
 }
 
 void MainWindow::on_C8_clicked()
 {
     sons("famD");
+    log("famD");
 
 }
 
@@ -192,10 +213,12 @@ void MainWindow::on_C9_clicked()
 {
 
     sons("solmD");
+    log("solmD");
 }
 
 void MainWindow::on_C_10_clicked()
 {
     sons("lamD");
+    log("lamD");
 
 }
